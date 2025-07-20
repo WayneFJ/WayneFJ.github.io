f329845e66e5b63545668eb3460b5be121062d07
WayneFJ <97973336+WayneFJ@users.noreply.github.com>
2025-07-20 14:32:58 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/WayneFJ.github.io/", // Add this line for GitHub Pages
+  base: "./", // Add this line for GitHub Pages
   server: {
     host: "::",
     port: 8080,
